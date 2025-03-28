c50b4d9524b567c1e30d81b1a0443e06eafe56ad
Saood Ahmad <saoodchoudhary9@gmail.com>
2025-03-28 09:30:22 +0530

    update

--- src/app/(home)/services/layout.js
+++ src/app/(home)/services/layout.js
@@ -3,17 +3,22 @@ import BlogSection from '@/components/home/BlogSection'
 import Footer from '@/components/Layout/Footer'
 import React from 'react'
 
-const layout = ({children}) => {
-  return (
-    <div className='mt-[100px]'>
-        {children}
-        <VisaSolutions />
-        
-        <BlogSection />
+const layout = ({ children }) => {
+    return (
+        <div className='mt-[100px]'>
+            {children}
+            <VisaSolutions />
+            <div className="relative overflow-hidden">
 
-        <Footer />
-    </div>
-  )
+                {/* <div className="absolute blur-[200px] rounded-full top-[20%] -right-[14%]  w-[500px] h-[500px] bg-[#0B82E6] opacity-50"></div> */}
+                <div className="absolute blur-[200px] rounded-full top-[30%] md:top-[40%] -left-[14%]  w-[500px] h-[500px] bg-[#0B82E6] opacity-50"></div>
+                <BlogSection />
+                <div className="relative  z-30">
+                    <Footer />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default layout
--- src/app/page.js
+++ src/app/page.js
@@ -41,8 +41,9 @@ export default function Home() {
         {/* <div className="absolute blur-[200px] rounded-full top-[20%] -right-[14%]  w-[500px] h-[500px] bg-[#0B82E6] opacity-50"></div> */}
         <div className="absolute blur-[200px] rounded-full top-[30%] md:top-[40%] -left-[14%]  w-[500px] h-[500px] bg-[#0B82E6] opacity-50"></div>
         <BlogSection />
-
+<div className="relative  z-30">
         <Footer />
+        </div>
         </div>
     </div>
   );
