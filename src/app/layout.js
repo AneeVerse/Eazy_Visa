d23dcb85c9eca05368d8126de16aa7a9309ce92a
Aneeverse <83137173+AneeVerse@users.noreply.github.com>
2025-05-12 16:35:37 +0530

    gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import Footer from "@/components/Layout/Footer";
 import NextTopLoader from "nextjs-toploader";
 import FloatingActionButton from "@/components/Layout/FloatingActionButton";
 
-
 const comfortaa = Comfortaa({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -21,7 +20,6 @@ export const metadata = {
         url: '/favicon.ico',
         type: 'image/x-icon',
       },
-      // For modern browsers
       {
         url: '/favicon-16x16.png',
         sizes: '16x16',
@@ -45,22 +43,44 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${comfortaa.variable}`}>
       <head>
-     
+        {/* Google Tag Manager (GTM) Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-W9FX59F'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W9FX59F');
+            `,
+          }}
+        />
       </head>
-      <body className="font-sans">
-      <NextTopLoader
-         color="#0B82E6"
-         initialPosition={0.08}
-         height={3}
-         showSpinner={false}
-         easing="ease"
-         speed={500}
-         shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-         />
+      <body className="font-comfortaa">
+        {/* Google Tag Manager (GTM) NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9FX59F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Page Content */}
+        <NextTopLoader
+          color="#0B82E6"
+          initialPosition={0.08}
+          height={3}
+          showSpinner={false}
+          easing="ease"
+          speed={500}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
         <Navbar />
         <div className="mt-[80px]">
           {children}
-           <FloatingActionButton /> 
+          <FloatingActionButton />
         </div>
       </body>
     </html>
