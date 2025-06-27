62c05596831432a9d160f0e9a5e0cb95df9c7663
mayur5689 <your-email@example.com>
2025-06-27 17:21:25 +0530

    Update MetaPixel component to implement Meta Pixel tracking with a default pixel ID and improved initialization logging. Simplify the code structure and ensure NoScript fallback is always rendered.

--- src/components/common/MetaPixel.jsx
+++ src/components/common/MetaPixel.jsx
@@ -1,42 +1,37 @@
 "use client";
 import { useEffect } from 'react';
 
-const MetaPixel = ({ pixelId = 'YOUR_PIXEL_ID_HERE' }) => {
+const MetaPixel = ({ pixelId = '1366921161271735' }) => {
   useEffect(() => {
-    // Meta Pixel will be implemented here when pixel ID is provided by Shaheen
-    if (typeof window !== 'undefined' && pixelId !== 'YOUR_PIXEL_ID_HERE') {
-      console.log('Meta Pixel initialized:', pixelId);
+    if (typeof window !== 'undefined') {
+      // Meta Pixel Code
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      window.fbq('init', pixelId);
+      window.fbq('track', 'PageView');
       
-      // Meta Pixel base code will go here
-      // !function(f,b,e,v,n,t,s)
-      // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      // n.queue=[];t=b.createElement(e);t.async=!0;
-      // t.src=v;s=b.getElementsByTagName(e)[0];
-      // s.parentNode.insertBefore(t,s)}(window,document,'script',
-      // 'https://connect.facebook.net/en_US/fbevents.js');
-      // fbq('init', pixelId);
-      // fbq('track', 'PageView');
-    } else {
-      console.log('Meta Pixel placeholder - waiting for pixel ID from Shaheen');
+      console.log('Meta Pixel initialized with ID:', pixelId);
     }
   }, [pixelId]);
 
   return (
     <>
-      {/* Meta Pixel NoScript fallback will go here when implemented */}
-      {pixelId !== 'YOUR_PIXEL_ID_HERE' && (
-        <noscript>
-          <img 
-            height="1" 
-            width="1" 
-            style={{ display: 'none' }} 
-            src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
-      )}
+      {/* Meta Pixel NoScript fallback */}
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: 'none' }} 
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
     </>
   );
 };
