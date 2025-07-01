6d51d1d2ba38eb9b4cb5e248ad2fc3be352dcdc4
mayur5689 <your-email@example.com>
2025-07-01 12:11:35 +0530

    Add sitemap generation and robots.txt configuration for SEO optimization. Introduce next-sitemap package and update package.json scripts for post-build sitemap handling.

---
+++ next-sitemap.config.js
@@ -0,0 +1,20 @@
+const { getAllBlogs } = require('./src/lib/sanity');
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.eazyvisas.com',
+  generateRobotsTxt: true, // Generate a robots.txt file
+  sitemapSize: 5000,
+  outDir: './public', // Output directory for sitemap
+  generateIndex: false, // do not generate index, output only sitemap.xml
+  additionalPaths: async (config) => {
+    // Fetch all blog posts from Sanity
+    const blogs = await getAllBlogs();
+    return blogs.map((blog) => ({
+      loc: `/blogs/${blog.slug.current}`,
+      lastmod: blog.publishedAt,
+      changefreq: 'daily',
+      priority: 0.7,
+    }));
+  },
+}; 
